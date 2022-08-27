250ae8d6494567043b6303ff9721dd2a90759881
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-08-27 22:03:04 +0500

    expanding cards

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeActiveClass();
+    card.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
